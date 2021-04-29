7e43c25711db68183db946cb18e40adb8d25fc98
yuktibhatt <bhattyukti5@gmail.com>
2021-04-29 21:38:01 +0545

    first commit

---
+++ second/01var.js
@@ -0,0 +1,12 @@
+var fullName = "Yukti";
+var courseName = "React Js Bootcamp";
+var isLoggedIn = true;
+
+var loggedCount = 34;
+// console.log(loggedCount);
+
+
+
+var paymentMode;
+paymentMode = "Credit Card";
+console.log(paymentMode);
