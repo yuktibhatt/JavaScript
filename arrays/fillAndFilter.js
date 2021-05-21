10d2ada0e6194f04f2e0ab4e0f969295a5268162
yuktibhatt <bhattyukti5@gmail.com>
2021-05-21 16:06:44 +0545

    fill and fillers

---
+++ arrays/fillAndFilter.js
@@ -0,0 +1,13 @@
+var testArray = [2, 4, 5, 6, 7, 8, 9, 1];
+
+console.log(testArray.fill("y", 2, 5));  //from position 2 till 5 it fills with h
+
+const myNumbers = [24, 25, 26, 57, 78, 70];
+const result = myNumbers.filter( (num) => num < 50);
+
+
+console.log(result);
+
+
+
+
