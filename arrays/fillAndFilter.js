c69c529475558f21f0ba842d5a0cae9809f5e205
yuktibhatt <bhattyukti5@gmail.com>
2021-05-23 17:21:54 +0545

    slice and splice

--- arrays/fillAndFilter.js
+++ arrays/fillAndFilter.js
@@ -1,13 +1,18 @@
 var testArray = [2, 4, 5, 6, 7, 8, 9, 1];
 
-console.log(testArray.fill("y", 2, 5));  //from position 2 till 5 it fills with h
+//console.log(testArray.fill("y", 2, 5));  //from position 2 till 5 it fills with h
 
 const myNumbers = [24, 25, 26, 57, 78, 70];
 const result = myNumbers.filter( (num) => num < 50);
 
 
-console.log(result);
+//console.log(result);
 
+var users = ["Ted", "Tim", "Sam", "Ron", "Tin"];
+//console.log(users.slice(1,3));
 
+//console.log(users.slice(1));
 
+users.splice(1,2, "HI");  //starting point and how many should you count
+console.log(users);
 
