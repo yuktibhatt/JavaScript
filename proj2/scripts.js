8ed071df41f8d7907d81c0595a796989ea85b90b
yuktibhatt <bhattyukti5@gmail.com>
2021-05-24 17:43:56 +0545

    projects to understand DOM

--- proj2/scripts.js
+++ proj2/scripts.js
@@ -11,6 +11,28 @@ const getBGColor = (selectedElement) => {
 return window.getComputedStyle(selectedElement).backgroundColor;
 };
 
-console.log(getBGColor(pink));
+
+// var color = getBGColor(pink);
+
+// pink.addEventListener('mouseenter', () => {
+//     center.style.background = color;
+// })
+
+const magicColorChanger = (element, color) => {
+    return element.addEventListener('mouseenter', () => {
+        center.style.background = color;
+    });
+}
+
+magicColorChanger(red, getBGColor(red));
+magicColorChanger(orange, getBGColor(orange));
+magicColorChanger(cyan, getBGColor(cyan));
+magicColorChanger(violet, getBGColor(violet));
+magicColorChanger(pink, getBGColor(pink));
+
+
+
+
+
 
 
