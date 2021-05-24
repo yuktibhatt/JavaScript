d326505c0c766f59757ccf49a841923479c2b385
yuktibhatt <bhattyukti5@gmail.com>
2021-05-24 17:31:18 +0545

    projects to understand DOM

---
+++ proj2/scripts.js
@@ -0,0 +1,16 @@
+const red = document.querySelector(".red");
+const cyan = document.querySelector(".cyan");
+const violet = document.querySelector(".violet");
+const orange = document.querySelector(".orange");
+const pink = document.querySelector(".pink");
+const center = document.querySelector(".center");
+
+//console.log(window.getComputedStyle(red).background);
+
+const getBGColor = (selectedElement) => {
+return window.getComputedStyle(selectedElement).backgroundColor;
+};
+
+console.log(getBGColor(pink));
+
+
