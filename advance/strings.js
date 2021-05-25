1ba5a92e4e6bda1b5ccf7e923d15dc68775a4d2c
yuktibhatt <bhattyukti5@gmail.com>
2021-05-25 16:48:04 +0545

    some string operations

---
+++ advance/strings.js
@@ -0,0 +1,10 @@
+var name = `Yukti`;
+var fullName = `${name} Bhatt`;
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+
+console.log(fullName.endsWith("t"));
+console.log(fullName.includes("kti"));
+console.log(fullName.toUpperCase());
+
