cf4ccc5ab2cc88ae341c15132e2d38e8f9c2e2b6
yuktibhatt <bhattyukti5@gmail.com>
2021-05-25 16:41:43 +0545

    scope of let

---
+++ advance/scope.js
@@ -0,0 +1,10 @@
+//console.log(name);
+
+let name = "yukti";
+
+if (true) {
+    let lastName = "Bhatt";  //scope of et dies within this
+}
+console.log(lastName);  //you get an error because you can't access the variable let once its scope is over
+
+
