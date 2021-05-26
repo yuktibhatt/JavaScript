28a881ba94c32911c45b2b0d807d60b53a3475ca
yuktibhatt <bhattyukti5@gmail.com>
2021-05-26 12:54:29 +0545

    classes in js

--- advance/class.js
+++ advance/class.js
@@ -3,20 +3,27 @@ class User{
         this.name = name;
         this.email = email;
     }
-    courseList = [];
+    #courseList = [];  //private varaible
 
     getInfo(){
         return {name: this.name, email : this.email };
     }
     enrollCourse(name) {
-        this.courseList.push(name);
+        this.#courseList.push(name);
     }
     getCourseList(){
-        return this.courseList;
+        return this.#courseList;
     }
 }
 
 
 module.exports = User;
 
+
+const rock = new User("rock", "r@gmail.com");
+
+console.log(rock.getInfo());
+rock.enrollCourse("Angular");
+console.log(rock.getCourseList());
+console.log(rock.courseList);
 
