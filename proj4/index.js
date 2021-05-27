59e2bbc1673feceeb278e67fe8a5923818f6a706
yuktibhatt <bhattyukti5@gmail.com>
2021-05-27 12:28:10 +0545

    a simple game to guess smileys

--- proj4/index.js
+++ proj4/index.js
@@ -1,7 +1,7 @@
 const cards = document.querySelectorAll(".card");
 console.log(cards);
 
-//variables
+//variables 
 var isFlipped = false;
 var firstCard;
 var secondCard;
@@ -9,57 +9,56 @@ var secondCard;
 cards.forEach((card) => card.addEventListener("click", flip));
 
 function flip() {
-  //   console.log("Card flipped");
-  // console.log(this);
+  //console.log("Card Flipped");
   this.classList.add("flip");
-  if (!isFlipped) {
+  if(!isFlipped){
     isFlipped = true;
     firstCard = this;
   } else {
     secondCard = this;
     console.log(firstCard);
     console.log(secondCard);
-
     checkIt();
   }
 }
 
-function checkIt() {
-  //   console.log("Checking...");
-  if (firstCard.dataset.image === secondCard.dataset.image) {
+function checkIt(){
+  //console.log("Checking.. ");
+  if (firstCard.dataset.image === secondCard.dataset.image ) {
     success();
   } else {
     fail();
   }
 }
 
+
 function success() {
-  //   console.log("Success");
-  firstCard.removeEventListener("click", flip);
-  secondCard.removeEventListener("click", flip);
+  //console.log("Success..");
+  firstCard.removeEventListener('click', flip);
+  secondCard.removeEventListener('click', flip);
   reset();
 }
 
 function fail() {
-  //   console.log("Failed");
+  //console.log("Fail..");
   setTimeout(() => {
     firstCard.classList.remove("flip");
-    secondCard.classList.remove("flip");
+    secondCard.classList.remove("flip");  
     reset();
-  }, 1000);
+  } , 1000);
 }
 
 function reset() {
   isFlipped = false;
-  firstCard = null;
+  firstCard= null;
   secondCard = null;
-}
+} 
 
-//TODO: shuffle
-
-(function shuffle() {
+(function shuffle () {
   cards.forEach((card) => {
     var index = Math.floor(Math.random() * 16);
     card.style.order = index;
-  });
+  })
 })();
+
+
