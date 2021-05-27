15282685851f7f7c38632d11b968ba6df1f40d3e
yuktibhatt <bhattyukti5@gmail.com>
2021-05-27 11:06:01 +0545

    double shot

---
+++ advance/doubleshot.js
@@ -0,0 +1,15 @@
+const google = 'google';
+const fb = null;
+
+if (google) {
+    console.log("I execute- BLOCK 1");
+}
+
+if (!!fb) {    //double shot
+    console.log("I execute -BLOCK 2");
+}
+
+
+
+
+
