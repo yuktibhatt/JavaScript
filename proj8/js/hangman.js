4b58146a34b92b7ef7b5026767316a133e012166
yuktibhatt <bhattyukti5@gmail.com>
2021-06-10 19:15:23 +0545

    hangman

--- proj8/js/hangman.js
+++ proj8/js/hangman.js
@@ -19,6 +19,7 @@ let answer ="";
 let maxWrong = 6;
 let mistakes = 0;
 let guessed = [];
+let wordStatus = null;
 
 function randomWord() {
     answer = prog_lang[Math.floor(Math.random() * prog_lang.length)];
@@ -40,8 +41,67 @@ function generateButtons() {
     document.getElementById('keyboard').innerHTML = buttonsHTML;
   }
 
+  function handleGuess(chosenLetter) {
+    guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+    document.getElementById(chosenLetter).setAttribute('disabled', true);
+    
+    if(answer.indexOf(chosenLetter) >= 0){
+      guessedWord();
+      checkGameWon();
+
+    } else if (answer.indexOf(chosenLetter) === -1){
+      mistakes++;
+      updateMistakes();
+      checkGameLost();
+      updateHangmanPic();
+    }
+  }
+
+  function checkGameWon(){
+    if (wordStatus=== answer) {
+      document.getElementById('keyboard').innerHTML = 'You Won!';
+    }
+  }
+
+  function checkGameLost(){
+    if(mistakes === maxWrong){
+      document.getElementById('wordSpotlight').innerHTML = 'The answer was : ' + answer;
+      document.getElementById('keyboard').innerHTML = 'You lose :( ';
+    }
+  }
+
+  function updateHangmanPic() {
+    document.getElementById('hangmanPic').src = './images/' + mistakes + '.jpg';
+  }
+
+
+  function guessedWord() {
+    wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');
+    document.getElementById('wordSpotlight').innerHTML = wordStatus;
+    
+  }
+  function updateMistakes() {
+    document.getElementById('mistakes').innerHTML = mistakes;
+  }
+
+  function reset(){
+    mistakes = 0;
+    guessed =[];
+    document.getElementById('hangmanPic').src = './images/0.jpg';
+
+    randomWord();
+    guessedWord();
+    updateMistakes();
+    generateButtons();
+  }
+
+
+  document.getElementById('maxWrong').innerHTML = maxWrong;
+
+
 randomWord();
 generateButtons();
+guessedWord();
 
 
 
